b2f0cf48ee69a2fc5397f5608dd06f7939bbccbd
sko1208 <skosam1208@gmail.com>
2022-04-28 16:19:46 +0300

    Maked header

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// scrip fot burger menu
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+    
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
