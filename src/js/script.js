cbdff7e4d23499a751ac52439e1c5d5238b89450
sko1208 <skosam1208@gmail.com>
2022-05-18 00:00:56 +0300

    maked delicious block

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,52 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+
+
+
+var slider = tns ({
+    
+    
+        "container": "#vertical",
+        "items": 3,
+        "axis": "vertical",
+        "swipeAngle": false,
+        "speed": 400,
+        "gutter": 30,
+        "controls": false,
+        "nav": false,
+        "autoplay": true,
+        "autoplayButtonOutput": false,
+        "mouseDrag": true,
+        "autoplayTimeout": 5000,
+        "speed": 2000
+
+
+        
+      
+  });
+
+  var slider = tns ({
+    
+    
+    "container": "#vertica",
+    "items": 3,
+    "axis": "vertical",
+    "swipeAngle": false,
+    "speed": 400,
+    "gutter": 30,
+    "controls": false,
+    "nav": false,
+    "autoplay": true,
+    "autoplayButtonOutput": false,
+    "mouseDrag": true,
+    "autoplayTimeout": 3000,
+    "speed": 2000,
+    "autoplayDirection": 'backward'
+
+
+    
+  
+});
+  
